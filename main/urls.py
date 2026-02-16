90b21309359dc715473b26a34e3f78e7420c188d
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-16 11:39:39 +0500

    views and urls are added

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('admin/', admin.site.urls, name='first'),
-	path('', views.first_view),
-	path('teachers/', views.first_view),
-	path('students/', views.first_view)
+	path('', views.first_view, name='first'),
+	path('<str:page>/', views.pages, name='pages'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -6,9 +6,37 @@ def first_view(request):
     html = f"""
     
     <h1>Tashkent Information Technology University Fergana Branch</h1>
-    <a href="/teachers"> TEACHERS >> </a><br>
-    <a href="/students"> STUDENTS >> </a>
-    <a href=/first_page"> FIRST PAGE << </a>
-    
+    <a href="/teachers/"> TEACHERS >> </a><br>
+    <a href="/students/"> STUDENTS >> </a><br>
+    <!--<a href="/"> FIRST PAGE << </a> this is a comment -->
     """
+
+    return HttpResponse(html)
+
+
+def pages(request, page):
+    if page == 'teachers':
+        html = f"""
+        
+        <h1>{page}</h1>
+        <p>
+        A teacher, also called a schoolteacher or formally an educator, is a person who helps students to acquire knowledge, competence, or virtue, via the practice of teaching.
+        Informally the role of teacher may be taken on by anyone (e.g. when showing a colleague how to perform a specific task). In some countries, teaching young people of school age may be carried out in an informal setting, such as within the family (homeschooling), rather than in a formal setting such as a school or college. Some other professions may involve a significant amount of teaching (e.g. youth worker, pastor).
+        </p>
+        <a href="/"> << FIRST PAGE </a>
+        """
+    elif page == 'students':
+        html = f"""
+
+        <h1>{page}</h1>
+        <p>
+        A student is a person enrolled in a school or other educational institution, or more generally, a person who takes a special interest in a subject.[1]
+        In the United Kingdom and most commonwealth countries, a "student" attends a secondary school or higher (e.g., college or university); those in primary or elementary schools are "pupils".[2]
+        </p>
+        <a href="/"> << FIRST PAGE </a>
+        """
+
+    else:
+        html = "<h1>PAGE NOT FOUND</h1><a href="/"> << FIRST PAGE</a>"
+
     return HttpResponse(html)
