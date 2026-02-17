0a29c37dd6a83bb9f6b61ed943fe7c798a0af891
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-17 11:41:29 +0500

    added tables

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Students
+from .models import Students, Teachers, Faculty, Subjects
+
 # Register your models here.
 admin.site.register(Students)
+admin.site.register(Teachers)
+admin.site.register(Faculty)
+admin.site.register(Subjects)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import CharField
 from django.utils.text import slugify
 # Create your models here.
 
@@ -7,4 +8,37 @@ class Students(models.Model):
     last_name = models.CharField(max_length=100, blank=True, null=True)
     age = models.SmallIntegerField(blank=True, null=True)
     link = models.URLField(null=True)
+
+    def __str__(self):
+        return self.name.title()
+
+
+class Faculty(models.Model):
+    professor = models.CharField(max_length=100)
+    assigned_course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.professor.title()
+
+
+class Teachers(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    # degree = models.CharField(max_length=100, blank=True, null=True)
+    # email = models.CharField(max_length=150)
+    phone_number = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name.title()
+
+
+class Subjects(models.Model):
+    name = models.CharField(max_length=100)
+    credit = models.SmallIntegerField(max_length=150)
+    resources = models.SmallIntegerField(max_length=10)
+    tasks_number = models.SmallIntegerField(max_length=10)
+
+    def __str__(self):
+        return self.name.title()
+
 
