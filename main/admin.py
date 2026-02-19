a079e61af3420303f8fb4d3b65e041a2e0cccb3e
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-19 14:37:21 +0500

    outsourced db

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from .models import Students, Teachers, Faculty, Subjects, Finance
-
-# Register your models here.
-admin.site.register(Students)
-admin.site.register(Teachers)
-admin.site.register(Faculty)
-admin.site.register(Subjects)
-admin.site.register(Finance)
+# from django.contrib import admin
+# from .models import Students, Teachers, Faculty, Subjects, Finance
+#
+# # Register your models here.
+# admin.site.register(Students)
+# admin.site.register(Teachers)
+# admin.site.register(Faculty)
+# admin.site.register(Subjects)
+# admin.site.register(Finance)
